450e340388c1bfa748af5acdad940ec889fe75c4
Ritam Majjumdar <majumdar_ritam@yahoo.com>
2018-07-10 12:33:43 +0530

    Updated index.js, getRandomValue.js and isUpperCase.js

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ const { prompt, print } = require('./prompt-print');
  * 1. prompt function to get input from the user
  * 2. print function to print text on to the console
  */
+print('What do you want to say to Grandma!!');
+let countbye = 0;
+while (countbye < 2) {
+  const userinput = prompt();
+  if (userinput === 'BYE') {
+    countbye += 1;
+    print('NO, NOT SINCE '.concat(getRandomValue(1930, 1950)));
+  } else if (isUppercase(userinput)) {
+    print('NO, NOT SINCE '.concat(getRandomValue(1930, 1950)));
+  } else {
+    print('HUH?! SPEAK UP, SONNY!');
+  }
+}
--- isUpperCase.js
+++ isUpperCase.js
@@ -7,6 +7,20 @@
  * @param {string} s The string to be checked
  * @returns {boolean}
  */
-const isUppercase = (s) => {};
+const isUppercase = (s) => {
+  let i;
+  let flag = 0;
+  let char = '';
+  for (i = 0; i < s.length; i += 1) {
+    char = s.charAt(i);
+    if (char !== ' ') {
+      if (char === char.toLowerCase()) {
+        flag = 1;
+        break;
+      }
+    }
+  }
+  return (flag === 0);
+};
 
 module.exports = isUppercase;
